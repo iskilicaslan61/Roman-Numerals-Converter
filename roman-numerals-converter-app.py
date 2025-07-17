6c316579ddda50aa5abc34a7ed56f04d50ee3e88
Ismail <iskilicaslan61@gmail.com>
2025-07-17 09:37:00 +0200

    name updated

--- roman-numerals-converter-app.py
+++ roman-numerals-converter-app.py
@@ -25,7 +25,7 @@ def main_post():
             return render_template('index.html', developer_name='Sumeyra Dogan', not_valid=True)
         return render_template('result.html', number_decimal=number, number_roman=convert(number), developer_name='Ismail Kilicaslan')
     else:
-        return render_template('index.html', developer_name='Ismail Kilicaslan ', not_valid=False)
+        return render_template('index.html', developer_name='Sumeyra Dogan ', not_valid=False)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
